1c27abe85c49bf6287b3fe21ed0230b6085f6520
Hafsteinn Ragnarsson <Haffi921@users.noreply.github.com>
2023-02-12 08:23:48 +0000

    Updated test: TestHtmlParser

--- tests/util/test_html_parser.py
+++ tests/util/test_html_parser.py
@@ -3,12 +3,8 @@ from src.util import html_parser
 
 
 class TestHtmlParser(unittest.TestCase):
-
     def test_basic(self):
-        self.assertEqual(
-            html_parser.get_text("<strong>Hello</strong>"),
-            "Hello"
-        )
+        self.assertEqual(html_parser.get_text("<strong>Hello</strong>"), "Hello")
 
 
 if __name__ == "__main__":
