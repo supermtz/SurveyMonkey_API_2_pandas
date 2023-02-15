3d844c742beae4a13e9e3f3187afbc666b1a5dba
Hafsteinn Ragnarsson <Haffi921@users.noreply.github.com>
2023-02-15 16:34:21 +0000

    Change: Dictionary util

---
+++ src/utils/dictionary.py
@@ -0,0 +1,24 @@
+
+# def dictionary_path(dictionary: dict, path: str):
+#     """Get value from dictionary using path"""
+#     for key in path.split("/"):
+#         dictionary = dictionary[key]
+#     return dictionary
+
+def get_nested_value(data: dict, path: str) -> str | int | list | dict:
+    """Get a value from a nested path"""
+    path = path.split("/")
+
+    for key in path:
+        data = data[key]
+
+    return data
+
+def get_values(data: dict, *args) -> tuple[str | int | list | dict]:
+    """Get values from a nested dictionary"""
+    values = []
+
+    for arg in args:
+        values.append(data[arg])
+
+    return tuple(values)
